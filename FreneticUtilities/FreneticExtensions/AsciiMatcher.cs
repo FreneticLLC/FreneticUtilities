1a7b1fae56499993d5b7f75ec286b3ac8ba98caf
mcmonkey <agoodwin@freneticllc.com>
2018-11-17 14:48:08 -0800

    add an ascii validation matcher utility

---
+++ FreneticUtilities/FreneticExtensions/AsciiMatcher.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FreneticUtilities.FreneticExtensions
+{
+    /// <summary>
+    /// Helper class to match ASCII characters efficiently.
+    /// </summary>
+    public class AsciiMatcher
+    {
+        /// <summary>
+        /// Maximum value considered part of the ASCII range (127).
+        /// </summary>
+        public static readonly int MAX_ASCII = 127;
+
+        /// <summary>
+        /// Minimum value considered outside the ASCII range (128) (<see cref="MAX_ASCII"/> + 1).
+        /// </summary>
+        public static readonly int MIN_NON_ASCII = MAX_ASCII + 1;
+
+        /// <summary>
+        /// Array of booleans, sized as <see cref="MIN_NON_ASCII"/>, such that "Chars[c]" where 'c' is any ASCII character is the validity of that character.
+        /// </summary>
+        public bool[] Chars = new bool[MIN_NON_ASCII];
+        /// <summary>
+        /// Construct the matcher from a string of valid symbols.
+        /// </summary>
+        /// <param name="valid">The valid symbol string.</param>
+        public AsciiMatcher(string valid)
+        {
+            for (int i = 0; i < MIN_NON_ASCII; i++)
+            {
+                Chars[i] = false;
+            }
+            for (int i = 0; i < valid.Length; i++)
+            {
+                Chars[valid[i]] = true;
+            }
+        }
+
+        /// <summary>
+        /// Construct the matcher from a function that evaluates whether any symbol is valid.
+        /// </summary>
+        /// <param name="isMatch">The validation function.</param>
+        public AsciiMatcher(Func<char, bool> isMatch)
+        {
+            for (int i = 0; i < MIN_NON_ASCII; i++)
+            {
+                Chars[i] = isMatch((char)i);
+            }
+        }
+
+        /// <summary>
+        /// Returns whether a character is considered valid.
+        /// </summary>
+        /// <param name="c">The character to check.</param>
+        /// <returns>Whether it is valid.</returns>
+        public bool IsMatch(char c)
+        {
+            return c > MAX_ASCII ? false : Chars[c];
+        }
+
+        /// <summary>
+        /// Returns whether a string only contains matching symbols.
+        /// </summary>
+        /// <param name="s">The string to check.</param>
+        /// <returns>Whether it is exclusively valid.</returns>
+        public bool IsOnlyMatches(string s)
+        {
+            if (s == null)
+            {
+                return false;
+            }
+            for (int i = 0; i < s.Length; i++)
+            {
+                if (!IsMatch(s[i]))
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+    }
+}
