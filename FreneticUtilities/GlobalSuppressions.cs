86642aeaa9fd4a2acdf2418afa09dc76d2d46284
Alex "mcmonkey" Goodwin <git_commits@alexgoodwin.dev>
2022-03-24 14:18:44 -0700

    supressions file: disable ide0060 and add justification labels

--- FreneticUtilities/GlobalSuppressions.cs
+++ FreneticUtilities/GlobalSuppressions.cs
@@ -5,5 +5,6 @@
 
 using System.Diagnostics.CodeAnalysis;
 
-[assembly: SuppressMessage("Usage", "CA2211:Non-constant fields should not be visible")]
+[assembly: SuppressMessage("Usage", "CA2211:Non-constant fields should not be visible", Justification = "Counter-productive to quality code")]
+[assembly: SuppressMessage("Style", "IDE0060:Remove unused parameter", Justification = "Endless false marking of methods whose parameters are defined by delegate/Func/Action usage")]
 [assembly: SuppressMessage("CodeQuality", "IDE0079:Remove unnecessary suppression", Justification = "WTF MICROSOFT???")]
