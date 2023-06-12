5793924dd9c1e99fe98c2419e4d02dcd7a0725b4
Alex "mcmonkey" Goodwin <git_commits@alexgoodwin.dev>
2023-06-11 18:05:16 -0700

    nuget to 1.0.5

--- make-nuget.ps1
+++ make-nuget.ps1
@@ -1,1 +1,3 @@
 dotnet pack --configuration release
+
+# And then eg: dotnet nuget push .\FreneticUtilities\bin\Release\FreneticLLC.FreneticUtilities.1.0.5.nupkg --source https://api.nuget.org/v3/index.json
