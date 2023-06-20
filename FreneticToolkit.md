d93a127e19f8a5d071bd6be8773f560ded71edb7
Alex "mcmonkey" Goodwin <git_commits@alexgoodwin.dev>
2023-06-20 06:12:31 -0700

    AsyncAutoResetEvent

--- FreneticToolkit.md
+++ FreneticToolkit.md
@@ -6,10 +6,14 @@ Frenetic Toolkit is a collection of general utilities for use a variety of cases
 ## Included
 
 - **AsciiMatcher**: helper to match specified ASCII characters within strings in a very fast and clean way.
+- **AsyncAutoResetEvent**: like `AutoResetEvent` but awaitable. I don't know why this isn't built into .NET itself.
 - **EnumHelper**: helper for C# enumerations, including a more efficient `HasFlag` option than the standard .NET Framework `Enum.HasFlag` (though still not as fast as doing it with a manual bitflag comparison - needs improvement, ideally from the C# team at Microsoft).
+- **ILGeneratorTracker**: helper to replace .NET's `ILGenerator` with one that's capable of input tracking and error checking.
 - **FreneticEvent**: an event handler system, including special helpers for things like synchronous pausable firings and other handy 'script-like' features.
-- **LockObject**: Just a handy tiny little empty object class to use with C# `lock` syntax.
+- **LockObject**: just a handy tiny little empty object class to use with C# `lock` syntax.
+- **MathHelper**: some handy basic math functions not built into .NET.
 - **ObjectConversionHelper**: helper for converting arbitrary object input to and from various common types.
 - **PrimitiveConversionHelper**: helper for converting raw byte array input to and from various primitive types.
+- **ResizableArray**: special optimization hack, essentially a `List<T>` that allows direct access to the underlying array.
+- **SpecialTools**: special fixes for .NET issues, notably broken localization behavior in .NET (eg `float.Parse((1.5f).ToString())` returns an error in some cultures, and the only sane solution is to turn autocultures off so C# behaves predictably, ie so that C# will work in non-American locations. The fact that we have to *disable* localization to have code reliably work in different localities is concerning - Microsoft needs to get their code in order).
 - **StringConversionHelper**: helper for converting string input to and from various common types.
-- **ILGeneratorTracker**: helper to replace .NET's `ILGenerator` with one that's capable of input tracking and error checking.
---
+++ FreneticUtilities/FreneticToolkit/AsyncAutoResetEvent.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace FreneticUtilities.FreneticToolkit;
+
+// This class is based on publications from Stephen Toub on MSFT among others
+// I cannot explain how or why one of .NET's core engineers published reference implementations of such an obvious foundation class without just implementing directly into .NET.
+// There's even a version implemented in VisualStudio sources. Just not in mainline .NET. Why???
+
+/// <summary>Like a <see cref="AutoResetEvent"/> but awaitable. Allows one thread to proceed per signal.</summary>
+public class AsyncAutoResetEvent
+{
+    /// <summary>Internal data, do not touch.</summary>
+    public struct InternalData
+    {
+        /// <summary>List of tasks representing the set of all threads waiting on this event.</summary>
+        public LinkedList<TaskCompletionSource<bool>> Waiting;
+
+        /// <summary>If true, the event has been signaled (and there were no current waiters).</summary>
+        public bool IsSignaled;
+
+        /// <summary>Access lock.</summary>
+        public LockObject Lock;
+    }
+
+    /// <summary>Internal data, do not touch.</summary>
+    public InternalData Internal;
+
+    /// <summary>Initialize the <see cref="AsyncAutoResetEvent"/>.</summary>
+    /// <param name="signaled">If true, is pre-signalled. If false, is not.</param>
+    public AsyncAutoResetEvent(bool signaled)
+    {
+        Internal.Waiting = new();
+        Internal.Lock = new();
+        Internal.IsSignaled = signaled;
+    }
+
+    /// <summary>Returns a task that waits for the next signal.</summary>
+    /// <param name="timeout">The max timeout before giving up.</param>
+    /// <param name="cancel">Cancellation token to allow stopping early from an arbitrary signal.</param>
+    /// <returns>True if signalled, false if timed out or cancelled.</returns>
+    public async Task<bool> WaitAsync(TimeSpan timeout, CancellationToken cancel = default)
+    {
+        LinkedListNode<TaskCompletionSource<bool>> node;
+        lock (Internal.Lock)
+        {
+            if (Internal.IsSignaled)
+            {
+                Internal.IsSignaled = false;
+                return true;
+            }
+            node = Internal.Waiting.AddLast(new TaskCompletionSource<bool>());
+        }
+        Task result = await Task.WhenAny(node.Value.Task, Task.Delay(timeout, cancel));
+        if (result == node.Value.Task)
+        {
+            return true;
+        }
+        else
+        {
+            lock (Internal.Lock)
+            {
+                Internal.Waiting.Remove(node);
+            }
+            return false;
+        }
+    }
+
+    /// <summary>Sets the signal, allowing exactly one thread through.</summary>
+    public void Set()
+    {
+        lock (Internal.Lock)
+        {
+            if (Internal.Waiting.Count > 0)
+            {
+                Internal.Waiting.First.Value.SetResult(true);
+                Internal.Waiting.RemoveFirst();
+            }
+            else
+            {
+                Internal.IsSignaled = true;
+            }
+        }
+    }
+}
